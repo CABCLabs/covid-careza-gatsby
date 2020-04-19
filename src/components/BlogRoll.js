6b2e98d47986ef40cb72d6bb9f974d1f58ee188c
thebrandconsultant <james@thebrandconsultant.co.za>
2020-04-19 15:36:12 +0200

    Update Blog Image Fix

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -8,39 +8,66 @@ class BlogRoll extends React.Component {
     const { data } = this.props;
     const { edges: posts } = data.allMarkdownRemark;
 
+    // This groups all teh posts by day
+    // Is somewhat relies on JS objects having a stable insertion key ordering
+    // But that is fine these days
+    const postGroups = posts.reduce((groups, { node: post }) => {
+      const date = post.frontmatter.date.substr(0, 10);
+
+      if (!groups.hasOwnProperty(date)) {
+        groups[date] = [];
+      }
+      groups[date].push(post);
+      return groups;
+    }, {});
+
     return (
-      <div className="columns is-multiline">
-        {posts &&
-          posts.map(({ node: post }) => (
-            <div className="is-parent column is-3" key={post.id}>
-              <article
-                className={`blog-list-item tile is-child box notification ${
-                  post.frontmatter.featuredpost ? "is-featured" : ""
-                }`}
-              >
-                <header>
-                  {post.frontmatter.featuredimage ? (
-                    <div className="featured-thumbnail">
-                      <PreviewCompatibleImage
-                        imageInfo={{
-                          image: post.frontmatter.featuredimage,
-                          alt: `featured image thumbnail for post ${post.frontmatter.title}`
-                        }}
-                      />
+      <div>
+        {Object.entries(postGroups).map(([date, posts]) => {
+          console.log(date, posts);
+          return (
+            <div className="blog-day-block">
+              <h4 className="content">{date}</h4>
+              <div className="columns is-multiline">
+                {posts &&
+                  posts.map((post) => (
+                    <div className="is-parent column is-4" key={post.id}>
+                      <article
+                        className={`blog-list-item tile is-child box notification ${
+                          post.frontmatter.featuredpost ? "is-featured" : ""
+                        }`}
+                      >
+                        <header>
+                          {post.frontmatter.featuredimage ? (
+                            <div className="featured-thumbnail">
+                              <PreviewCompatibleImage
+                                imageInfo={{
+                                  image: post.frontmatter.featuredimage,
+                                  alt: `featured image thumbnail for post ${post.frontmatter.title}`,
+                                }}
+                              />
+                            </div>
+                          ) : null}
+                        </header>
+                        <p className="post-meta">
+                          <Link
+                            className="title has-text-primary is-size-4"
+                            to={post.fields.slug}
+                          >
+                            {post.frontmatter.title}
+                          </Link>
+                          {/* <span className="subtitle is-size-5 is-block">
+                            {post.frontmatter.date}
+                          </span> */}
+                        
+                        </p>
+                      </article>
                     </div>
-                  ) : null}
-                  <p className="post-meta">
-                    <Link
-                      className="is-size-4"
-                      to={post.fields.slug}
-                    >
-                      {post.frontmatter.title}
-                    </Link>
-                  </p>
-                </header>
-              </article>
+                  ))}
+              </div>
             </div>
-          ))}
+          );
+        })}
       </div>
     );
   }
@@ -49,9 +76,9 @@ class BlogRoll extends React.Component {
 BlogRoll.propTypes = {
   data: PropTypes.shape({
     allMarkdownRemark: PropTypes.shape({
-      edges: PropTypes.array
-    })
-  })
+      edges: PropTypes.array,
+    }),
+  }),
 };
 
 export default () => (
@@ -72,11 +99,11 @@ export default () => (
               frontmatter {
                 title
                 templateKey
-                date(formatString: "MMMM DD, YYYY")
+                date(formatString: "DD/MM/YYYY")
                 featuredpost
                 featuredimage {
                   childImageSharp {
-                    fluid(maxWidth: 120, quality: 100) {
+                    fluid(maxWidth: 500, quality: 100) {
                       ...GatsbyImageSharpFluid
                     }
                   }
