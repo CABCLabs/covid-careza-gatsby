643466ba8408f75c0be1d0fdeff4b8b5cc42da42
thebrandconsultant <james@thebrandconsultant.co.za>
2020-09-17 16:55:18 +0200

    Update EntryRoll.js

--- src/components/EntryRoll.js
+++ src/components/EntryRoll.js
@@ -1,67 +1,63 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { Link, graphql, StaticQuery } from "gatsby";
-import PreviewCompatibleImage from "./PreviewCompatibleImage";
-
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link, graphql, StaticQuery } from 'gatsby'
+import PreviewCompatibleImage from './PreviewCompatibleImage'
+function shuffleArray(array) {
+  let i = array.length - 1;
+  for (; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array.slice(0, 4);;
+  
+}
 class EntryRoll extends React.Component {
   render() {
-    const { data } = this.props;
-    const { edges: posts } = data.allMarkdownRemark;
-    function shuffle(array) {
-      return [...array].sort(() => Math.random() - 0.5);
-  }
-
-  const sortedArray = shuffle(posts);
+    const { data } = this.props
+    const { edges: posts } = data.allMarkdownRemark
+    const shuffledPosts = shuffleArray(posts);
 
     return (
-      <div>
-        {Object.entries(postGroups).map(([date, sortedArray]) => {
-          console.log(date, posts);
-          return (
-            <div className="blog-day-block">
-              <h4 className="content">{date}</h4>
-              <div className="columns is-multiline">
-                {sortedArray &&
-                  sortedArray.map((post) => (
-                    <div className="is-parent column is-4" key={post.id}>
-                      <article
-                        className={`blog-list-item tile is-child box notification ${
-                          post.frontmatter.featuredpost ? "is-featured" : ""
-                        }`}
-                      >
-                        <header>
-                          {post.frontmatter.imageEntry ? (
-                            <div className="featured-thumbnail">
-                              <PreviewCompatibleImage
-                                imageInfo={{
-                                  image: post.frontmatter.imageEntry,
-                                  alt: `featured image thumbnail for post ${post.frontmatter.title}`,
-                                }}
-                              />
-                            </div>
-                          ) : null}
-                        </header>
-                        <p className="post-meta">
-                          <Link
-                            className="title has-text-primary is-size-4"
-                            to={post.fields.slug}
-                          >
-                            {post.frontmatter.title}
-                          </Link>
-                          {/* <span className="subtitle is-size-5 is-block">
-                            {post.frontmatter.date}
-                          </span> */}
-                        
-                        </p>
-                      </article>
-                    </div>
-                  ))}
-              </div>
-            </div>
-          );
-        })}
+      <div className="columns is-multiline">
+        {shuffledPosts &&
+          shuffledPosts.map(({ node: post }) => (
+            <div className="is-parent column is-4" key={post.id}>
+            <article
+              className={`blog-list-item tile is-child box notification ${
+                post.frontmatter.featuredpost ? "is-featured" : ""
+              }`}
+            >
+              <header>
+                {post.frontmatter.imageEntry ? (
+                  <div className="featured-thumbnail">
+                    <PreviewCompatibleImage
+                      imageInfo={{
+                        image: post.frontmatter.imageEntry,
+                        alt: `featured image thumbnail for post ${post.frontmatter.title}`,
+                      }}
+                    />
+                  </div>
+                ) : null}
+              </header>
+              <p className="post-meta">
+                <Link
+                  className="title has-text-primary is-size-4"
+                  to={post.fields.slug}
+                >
+                  {post.frontmatter.title}
+                </Link>
+                {/* <span className="subtitle is-size-5 is-block">
+                  {post.frontmatter.date}
+                </span> */}
+              
+              </p>
+            </article>
+          </div>
+          ))}
       </div>
-    );
+    )
   }
 }
 
@@ -71,14 +67,13 @@ EntryRoll.propTypes = {
       edges: PropTypes.array,
     }),
   }),
-};
+}
 
 export default () => (
   <StaticQuery
     query={graphql`
       query EntryRollQuery {
         allMarkdownRemark(
-          limit: 3
           sort: { order: DESC, fields: [frontmatter___date] }
           filter: { frontmatter: { station: { eq: "radio1" } } }
         ) {
@@ -95,9 +90,10 @@ export default () => (
                 templateKey
                 date(formatString: "DD/MM/YYYY")
                 featuredpost
+                station
                 imageEntry {
                   childImageSharp {
-                    fluid(maxWidth: 2048, quality: 100) {
+                    fluid(maxWidth: 500, quality: 100) {
                       ...GatsbyImageSharpFluid
                     }
                   }
@@ -110,4 +106,4 @@ export default () => (
     `}
     render={(data, count) => <EntryRoll data={data} count={count} />}
   />
-);
+)
