461b22008adecfbce3ea4799415380f928c46aaa
thebrandconsultant <james@thebrandconsultant.co.za>
2020-10-05 09:53:41 +0200

    Image

--- src/templates/entry.js
+++ src/templates/entry.js
@@ -122,7 +122,13 @@ export const pageQuery = graphql`
         title
         description
         tags
-        imageEntry 
+        imageEntry {
+          childImageSharp {
+            fluid(maxWidth: 500, quality: 100) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
         VideoEntryUrl
         TextEntry
         station
